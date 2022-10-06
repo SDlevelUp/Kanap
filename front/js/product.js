a11a43735994094d3d05da9d530f17f08da26633
Sarah Berri <sarahberri@outlook.fr>
2022-10-06 17:01:40 +0200

    product

--- front/js/product.js
+++ front/js/product.js
@@ -1,9 +1,19 @@
+//Redirection de l'URL des canapés vers la page d'ajout au panier
 let productId = new URL(window.location.href).searchParams.get('id')
     console.log(productId)
 
+
+// Utilisation de la méthode fetch qui permet d'exécuter des requêtes HTTP sans avoir besoin de recharger la page du navigateur
+
 fetch("http://localhost:3000/api/products/" + productId)
+
+// Envoie une réponse JSON au "composant" choisi
+
   .then((res) => res.json())
   .then((res) => manipPanier(res))
+
+
+// Création de la fonction pour ajouter la description des canapés au panier
 
 function manipPanier(canapé) {
     const {altTxt, colors, description, imageUrl, name, price} = canapé
@@ -11,6 +21,7 @@ function manipPanier(canapé) {
     addTitle(name)
     addPrice(price)
     addDescription(description)
+    addColors(colors)
 }
 
 function addImage(imageUrl, altTxt) {
@@ -44,4 +55,18 @@ function addDescription(description) {
     }
 }
 
+function addColors(colors) {
+    const select = document.querySelector("#colors")
+    if(select != null) {
+    colors.forEach((color) => {
+        const option = document.createElement("option")
+    option.value = color
+    option.textContent = color
+    select.appendChild(option)
+    console.log(color)
+        })
+    }
+}
 
+    
+    
